e8e249fcf97e75a183531dcad6970970161e56f8
victor297 <davidvictor297.com>
2023-12-11 17:37:58 +0100

    faq mock data

---
+++ utils/faq.ts
@@ -0,0 +1,56 @@
+const faqDataMock = [
+    {
+      category: "General",
+      items: [
+        {
+          question: "What is the company's long-term vision and goals?",
+          answer: "Our company's long-term vision is to create innovative solutions that positively impact people's lives. We aim to achieve this by fostering a culture of creativity, collaboration, and continuous improvement. Our primary goal is to become a leading force in the industry, recognized for our commitment to excellence and customer satisfaction."
+        },
+        {
+          question: "How does the company contribute to social responsibility?",
+          answer: "We are committed to corporate social responsibility and actively engage in initiatives that contribute to the well-being of society. From environmental sustainability to community outreach programs, we strive to make a positive impact. We believe that by integrating social responsibility into our business practices, we can create a better future for everyone."
+        },
+      ],
+    },
+    {
+      category: "Products",
+      items: [
+        {
+          question: "Can you provide an overview of Product X's advanced features?",
+          answer: "Product X is equipped with a range of advanced features designed to meet the diverse needs of our users. From cutting-edge technology to user-friendly interfaces, Product X stands out in the market. Some key features include customizable workflows, real-time analytics, and seamless integration with third-party applications."
+        },
+        {
+          question: "How frequently are product updates released?",
+          answer: "We are dedicated to continuously improving our products. Our team works tirelessly to release regular updates, addressing user feedback, enhancing security, and introducing new features. Updates are typically scheduled quarterly, with additional patches and enhancements rolled out as needed to ensure our users always have access to the latest and greatest."
+        },
+      ],
+    },
+    {
+      category: "Account",
+      items: [
+        {
+          question: "What security measures are in place to protect user accounts?",
+          answer: "We prioritize the security of our users' accounts. Our platform employs industry-standard encryption protocols to safeguard sensitive information. Additionally, we regularly conduct security audits and invest in the latest security technologies to stay ahead of potential threats. Users can also enable two-factor authentication for an extra layer of protection."
+        },
+        {
+          question: "How can I recover a locked account?",
+          answer: "If your account is locked, you can initiate the account recovery process by visiting our official website and following the 'Account Recovery' link. We will guide you through the necessary steps to verify your identity and unlock your account. For additional assistance, our customer support team is available 24/7 to provide guidance and support."
+        },
+      ],
+    },
+    {
+      category: "Billing",
+      items: [
+        {
+          question: "What payment methods are accepted for subscription billing?",
+          answer: "We accept a variety of payment methods to accommodate our users. This includes major credit cards, debit cards, and popular digital payment platforms. Users can conveniently manage their payment preferences through the billing section of their account dashboard. Our secure payment gateway ensures that transactions are processed safely and efficiently."
+        },
+        {
+          question: "Is there an option for annual billing with discounted rates?",
+          answer: "Yes, we offer an annual billing option for users who prefer a longer-term commitment. Choosing annual billing not only provides cost savings compared to monthly billing but also ensures uninterrupted access to premium features. Users can switch between billing cycles at any time to align with their preferences and budgetary considerations."
+        },
+      ],
+    },
+    // Add more categories and items as needed
+  ];
+  
