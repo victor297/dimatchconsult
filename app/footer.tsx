a5e80d166a4bd77d84aac3078bcb2009a302507b
esther-inyang <estherinyang07@gmail.com>
2023-12-19 16:36:57 +0100

    styled the navbar and footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -14,54 +14,56 @@ const Footer = () => {
   return (
     <footer className="py-6 bg-darkGray">
       <div className="w-[70%] m-auto">
-        <section className="flex items-center justify-between">
-          <div className="flex items-center gap-1">
+        <section className="lg:flex lg:justify-between">
+          <div className="flex items-center justify-center lg:justify-start gap-2 -ml-10 lg:gap-1 lg:-ml-0">
             <div>
-              <Image src={trenovaLogo} alt="logo" width={25} />
+              <Image src={trenovaLogo} alt="logo" width={30} />
             </div>
             <h2 className="text-white font-poppinsBold text-xl">
               WhiteCo<span className="text-blue">ll</span>ar
             </h2>
           </div>
 
-          <div className="flex items-center gap-20">
-            <div className="flex items-center gap-2">
-              <div>
-                <Image src={phoneIcon} alt="phone" width={50} />
+          <div className="flex flex-col lg:flex-row items-center md:justify-between md:items-baseline">
+            <div className="flex flex-col gap-4 mt-5 md:flex-row md:mt-0 md:gap-20">
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image src={phoneIcon} alt="phone" width={50} />
+                </div>
+                <div>
+                  <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
+                    Call Us
+                  </h2>
+                  <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
+                    +01 469 896 654
+                  </p>
+                </div>
               </div>
-              <div>
-                <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
-                  Call Us
-                </h2>
-                <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
-                  +01 469 896 654
-                </p>
+              <div className="flex items-center gap-2">
+                <Link href="">
+                  <Image src={messageIcon} alt="message" width={50} />
+                </Link>
+                <div>
+                  <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
+                    Mail Us
+                  </h2>
+                  <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
+                    info@Example.com
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <Link href="">
-                <Image src={messageIcon} alt="message" width={50} />
-              </Link>
-              <div>
-                <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
-                  Mail Us
-                </h2>
-                <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
-                  info@Example.com
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <div>
-                <Image src={locationIcon} alt="" width={50} />
-              </div>
-              <div>
-                <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
-                  Location
-                </h2>
-                <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
-                  Amsterdam, 109-74
-                </p>
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image src={locationIcon} alt="" width={50} />
+                </div>
+                <div>
+                  <h2 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
+                    Location
+                  </h2>
+                  <p className="text-white text-sm font-extralight mt-1 font-interRegular tracking-widest">
+                    Amsterdam, 109-74
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -69,16 +71,16 @@ const Footer = () => {
 
         <div className="border border-solid border-gray-600 my-6"></div>
 
-        <section className="flex justify-between">
-          <div className="flex gap-20">
+        <section className="flex flex-col items-center lg:flex-row md:justify-between lg:items-left">
+          <div className=" flex flex-col gap-10  items-center lg:flex-row md:gap-20">
             <div>
               <div className="flex gap-2 items-center">
                 <div className="w-3 h-3 bg-blue"></div>
-                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-sm">
+                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-lg lg:text-sm">
                   Quick Link
                 </h2>
               </div>
-              <div className="flex flex-col gap-3 mt-4">
+              <div className="flex flex-col items-center lg:items-baseline gap-3 mt-4">
                 <Link
                   href="/"
                   className="text-white text-sm font-interRegular tracking-widest font-light"
@@ -99,7 +101,7 @@ const Footer = () => {
                 </Link>
                 <Link
                   href="/services"
-                  className="text-white text-sm font-interRegular tracking-widest font-light"
+                  className="text-white text-sm font-interRegular tracking-widest font-light "
                 >
                   Services
                 </Link>
@@ -111,14 +113,14 @@ const Footer = () => {
                 </Link>
               </div>
             </div>
-            <div>
+            <div className="flex gap-2 flex-col items-center lg:items-baseline">
               <div className="flex gap-2 items-center">
                 <div className="w-3 h-3 bg-blue"></div>
-                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-sm">
+                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-lg lg:text-sm">
                   Our Services
                 </h2>
               </div>
-              <div className="flex flex-col gap-3 mt-4">
+              <div className="flex flex-col items-center lg:items-baseline gap-3 mt-4">
                 <Link
                   href="/"
                   className="text-white text-sm font-interRegular tracking-widest font-light"
@@ -154,12 +156,12 @@ const Footer = () => {
             <div>
               <div className="flex gap-2 items-center">
                 <div className="w-3 h-3 bg-blue"></div>
-                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-sm">
+                <h2 className="text-green font-poppinsSemiBold font-bold tracking-widest text-lg lg:text-sm">
                   Utility Page
                 </h2>
               </div>
 
-              <div className="flex flex-col gap-3 mt-4">
+              <div className="flex flex-col items-center lg:items-baseline gap-3 mt-4">
                 <Link
                   href="/"
                   className="text-white text-sm font-interRegular tracking-widest font-light"
@@ -194,60 +196,74 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="bg-gray-700 p-10">
-            <h2 className="text-white font-semibold tracking-widest text-sm">
-              Subscribe
-            </h2>
-            <p className="text-white font-extralight tracking-wider text-sm mt-3 mb-5">
-              Join Our Mailing List to get our news
-            </p>
-            <form>
-              <input
-                type="email"
-                placeholder="your email address"
-                className="p-4"
-              />
-              <button
-                role="submit"
-                className="p-4 text-center text-white font-light text-base tracking-widest bg-blue"
-              >
+          <div className="bg-gray-700 p-10 mt-8 lg:mt-0">
+            <div className="text-center md:text-left">
+              <h2 className="text-white font-semibold tracking-widest text-xl md:text-lg">
                 Subscribe
-              </button>
+              </h2>
+            </div>
+            <div className="text-center md:text-left">
+              <p className="text-white font-extralight tracking-wider text-sm mt-3 mb-5">
+                Join Our Mailing List to get our news
+              </p>
+            </div>
+            <form>
+              <div className="flex flex-col lg:flex-row lg:gap-0 justify-center items-center gap-4">
+                <div>
+                  <input
+                    type="email"
+                    placeholder="your email address"
+                    className="flex-1 md:w-[100%] p-2 text-sm lg:p-4 lg:text-base"
+                  />
+                </div>
+                <div>
+                  <button
+                    role="submit"
+                    className="rounded-sm lg:rounded-none py-2 px-4 lg:p-4 text-center text-white font-light lg:text-base tracking-widest bg-blue"
+                  >
+                    Subscribe
+                  </button>
+                </div>
+              </div>
             </form>
           </div>
         </section>
 
         <div className="border border-solid border-gray-600 my-6"></div>
 
-        <section className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <span className="text-white text-sm font-interRegular font-light">
-              Copyright
-            </span>
-            <span className="text-white mx-1 text-sm font-interRegular">
-              &copy;
-            </span>
-            <Link
-              href=""
-              className="text-green text-sm font-interRegular font-light"
-            >
-              WhiteCollar
-            </Link>{" "}
-            <span className="text-white">|</span>
-            <span className="text-white text-sm font-interRegular font-light">
-              {" "}
-              Designed by{" "}
-            </span>
-            <Link
-              href=""
-              className="text-blue text-sm font-interRegular font-light"
-            >
-              VictorFlow
-            </Link>
-            <span className="text-white text-sm font-interRegular font-light">
-              Powered by - Webflow
-            </span>
+        <section className="flex flex-col items-center md:flex-row md:justify-between">
+          <div className="flex flex-col lg:flex-row">
+            <div className="md:flex md:items-center md:gap-2">
+              <span className="text-white text-sm font-interRegular font-light">
+                Copyright
+              </span>
+              <span className="text-white mx-1 text-sm font-interRegular">
+                &copy;
+              </span>
+              <Link
+                href=""
+                className="text-green text-sm font-interRegular font-light"
+              >
+                WhiteCollar
+              </Link>
+              <span className="text-white mx-1">|</span>
+              <span className="text-white text-sm font-interRegular font-light">
+                Designed by
+              </span>
+            </div>
+            <div>
+              <Link
+                href=""
+                className="text-blue text-sm font-interRegular font-light px-2"
+              >
+                VictorFlow
+              </Link>
+              <span className="text-white text-sm font-interRegular font-light">
+                Powered by - Webflow
+              </span>
+            </div>
           </div>
+
           <div className="flex gap-3 items-center">
             <h3 className="text-white text-sm font-poppinsBold font-bold tracking-widest">
               Follow:
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { IconContext } from "react-icons";
+import { IoClose } from "react-icons/io5";
+import { HiMenuAlt2 } from "react-icons/hi";
 
 import locationIcon from "@/public/icons/nav-location.svg";
 import messageIcon from "@/public/icons/nav-message.svg";
@@ -14,7 +18,13 @@ import twitterIcon from "@/public/icons/twitter.svg";
 import pintrestIcon from "@/public/icons/pintrest.svg";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
   const pathname = usePathname();
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
 
   const menuItems = [
     {
@@ -50,90 +60,155 @@ const Navbar = () => {
   ];
 
   return (
-    <header>
-      <div className="flex justify-around bg-darkGray">
-        <div className="flex items-center gap-20 p-2">
-          <div className="flex items-center gap-3">
+    // <header className="bg-darkGray py-2 w-full lg:px-12 lg:py-3 text-white">
+    <header className="">
+      <div className="flex justify-end md:flex-none">
+        <button
+          className="block sm:hidden transition py-4 px-5 sm:py-0"
+          onClick={handleClick}
+        >
+          <IconContext.Provider
+            value={{
+              color: "black",
+              className: "global-class-name text-darkGray text-lg",
+              size: "30",
+            }}
+          >
             <div>
-              <Image src={locationIcon} alt="location" width={16} height={17} />
+              <HiMenuAlt2 />
             </div>
-            <div className=" text-white font-interRegular text-sm tracking-wide">
-              <p>24 Olive Street, Prairie, NY 53590</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-3">
-            <Link href="">
-              <Image src={messageIcon} alt="message" width={18} height={17} />
-            </Link>
-            <div>
-              <p className=" text-white font-interRegular text-sm tracking-wider">
-                whitecollar@gmail.com
-              </p>
-            </div>
-          </div>
+          </IconContext.Provider>
+        </button>
+      </div>
+
+      <div
+        className={`${
+          clicked
+            ? "block w-[100%] h-[100vh] bg-darkGray z-10 absolute top-0 transition ease-in-out delay-300 duration-300 "
+            : "hidden "
+        } md:block`}
+      >
+        <div className="flex justify-end md:flex-none">
+          <button
+            className="block sm:hidden transition py-4 px-5 sm:py-0 absolute top-0"
+            onClick={handleClick}
+          >
+            <IconContext.Provider
+              value={{
+                color: "white",
+                className: "global-class-name text-white text-lg",
+                size: "30",
+              }}
+            >
+              <div>
+                <IoClose />
+              </div>
+            </IconContext.Provider>
+          </button>
         </div>
 
-        <div className="flex items-center gap-20">
-          <div className="flex items-center gap-3">
-            <div>
-              <Image src={timeIcon} alt="time" width={18} height={17} />
+        <div className="pt-32 pb-10 lg:pt-0 lg:pb-0 px-5 lg:px-0 lg:flex lg:justify-around bg-darkGray">
+          <div className="lg:flex lg:items-center lg:gap-20 p-2">
+            <div className="flex items-center justify-center lg:justify-start gap-2 lg:gap-3">
+              <div>
+                <Image
+                  src={locationIcon}
+                  alt="location"
+                  width={16}
+                  height={17}
+                />
+              </div>
+              <div className=" text-white font-interRegular text-sm tracking-wide">
+                <p>24 Olive Street, Prairie, NY 53590</p>
+              </div>
             </div>
-            <div className=" text-white font-interRegular text-sm tracking-wide">
-              <p>Mon-Fri:8:00 am - 5:00 pm</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-3">
-            <Link href="">
-              <Image
-                src={instagramIcon}
-                alt="instagram"
-                width={18}
-                height={17}
-              />
-            </Link>
-            <Link href="">
-              <Image src={facebookIcon} alt="facebook" width={18} height={17} />
-            </Link>
-            <Link href="">
-              <Image src={twitterIcon} alt="twitter" width={18} height={17} />
-            </Link>
-            <Link href="">
-              <Image src={pintrestIcon} alt="pintrest" width={18} height={17} />
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-center justify-around">
-        <div className="flex items-center gap-2">
-          <div>
-            <Image src={trenovaLogo} alt="logo" width={30} />
-          </div>
-          <div>
-            <h2 className="text-darkGray font-poppinsBold text-2xl">
-              WhiteCo<span className="text-blue">ll</span>ar
-            </h2>
-          </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <nav className="p-10 bg-white text-darkGray">
-            {menuItems.map(({ id, name, link }) => (
-              <Link
-                key={id}
-                href={link}
-                className={`${
-                  pathname === link
-                    ? "bg-gray-100 font-interSemiBold font-bold text-darkGray px-4 py-2 rounded-t-lg"
-                    : "text-darkGray px-4 py-2 rounded-t-lg tracking-tight font-interRegular"
-                }`}
-              >
-                {name}
+            <div className="flex items-center justify-center lg:justify-end gap-2 lg:gap-3">
+              <Link href="">
+                <Image src={messageIcon} alt="message" width={18} height={17} />
               </Link>
-            ))}
-          </nav>
-          <div>
-            <button className="bg-blue px-8 py-4 text-white font-cabinRegular text-lg tracking-wider hover:bg-lightBlue">
-              Get Consulting
-            </button>
+              <div>
+                <p className=" text-white font-interRegular text-sm tracking-wider">
+                  whitecollar@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col lg:flex-row items-center gap-2 lg:gap-20">
+            <div className="flex items-center gap-2 lg:gap-3">
+              <div>
+                <Image src={timeIcon} alt="time" width={18} height={17} />
+              </div>
+              <div className=" text-white font-interRegular text-sm tracking-wide">
+                <p>Mon-Fri:8:00 am - 5:00 pm</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 lg:gap-3">
+              <Link href="">
+                <Image
+                  src={instagramIcon}
+                  alt="instagram"
+                  width={18}
+                  height={17}
+                />
+              </Link>
+              <Link href="">
+                <Image
+                  src={facebookIcon}
+                  alt="facebook"
+                  width={18}
+                  height={17}
+                />
+              </Link>
+              <Link href="">
+                <Image src={twitterIcon} alt="twitter" width={18} height={17} />
+              </Link>
+              <Link href="">
+                <Image
+                  src={pintrestIcon}
+                  alt="pintrest"
+                  width={18}
+                  height={17}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="lg:flex lg:flex-row lg:items-center lg:justify-around">
+          <div className="absolute top-12 lg:relative lg:top-0 w-[100%] lg:w-auto m-auto mt-5 lg:m-0 p-4 lg:p-0">
+            <div className="  flex justify-center items-center gap-1 lg:gap-2">
+              <div>
+                <Image src={trenovaLogo} alt="logo" width={30} />
+              </div>
+              <div>
+                <h2 className="text-white lg:text-darkGray font-poppinsBold text-2xl">
+                  WhiteCo<span className="text-blue">ll</span>ar
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="w-auto flex flex-col gap-10 lg:gap-0 lg:flex-row items-center lg:justify-center">
+            <nav className="w-[80%] lg:w-auto text-center lg:text-base flex flex-col lg:flex-row lg:p-8 bg-white rounded-sm lg:rounded-none text-darkGray">
+              {menuItems.map(({ id, name, link }) => (
+                <Link
+                  key={id}
+                  href={link}
+                  onClick={handleClick}
+                  className={`${
+                    pathname === link
+                      ? "bg-gray-100 font-interSemiBold font-bold text-darkGray py-2 lg:px-4 lg:py-2 rounded-t-lg"
+                      : "text-darkGray py-2 lg:px-4 lg:py-2 rounded-t-lg tracking-tight font-interRegular"
+                  }`}
+                >
+                  {name}
+                </Link>
+              ))}
+            </nav>
+            <div>
+              <button className="bg-blue px-8 py-4 text-white font-cabinRegular text-lg tracking-wider rounded-sm hover:bg-lightBlue">
+                Get Consulting
+              </button>
+            </div>
           </div>
         </div>
       </div>
