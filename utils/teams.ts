2fe5dd3605cac67609e45c21597346bc062aec48
esther-inyang <estherinyang07@gmail.com>
2023-12-21 02:23:27 +0100

    fixed navbar issue and started team page

--- utils/teams.ts
+++ utils/teams.ts
@@ -1,30 +1,64 @@
-
- const teams =   [
-    {
-      "id": 1,
-      "name": "Engineering",
-      "teamLead": { "id": 101, "name": "John Doe", "position": "Team Lead", "image": "/photo.jpg" },
-      "members": [
-        { "id": 102, "name": "Jane Smith", "position": "UX Designer", "image": "url_to_image" },
-        { "id": 103, "name": "Bob Johnson", "position": "QA Engineer", "image": "url_to_image" }
-      ]
+const teams = [
+  {
+    id: 1,
+    name: "Engineering",
+    teamLead: {
+      id: 101,
+      name: "John Doe",
+      position: "Team Lead",
+      image: "/photo.jpg",
     },
-    {
-      "id": 2,
-      "name": "Marketing",
-      "teamLead": { "id": 201, "name": "Alice Brown", "position": "Marketing Manager", "image": "/about.jpg" },
-      "members": [
-        { "id": 202, "name": "Charlie Green", "position": "Content Writer", "image": "/team.jpg" }
-      ]
+    members: [
+      {
+        id: 102,
+        name: "Jane Smith",
+        position: "UX Designer",
+        image: "url_to_image",
+      },
+      {
+        id: 103,
+        name: "Bob Johnson",
+        position: "QA Engineer",
+        image: "url_to_image",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Marketing",
+    teamLead: {
+      id: 201,
+      name: "Alice Brown",
+      position: "Marketing Manager",
+      image: "/about.jpg",
     },
-    {
-      "id": 3,
-      "name": "Sales",
-      "teamLead": { "id": 301, "name": "David White", "position": "Sales Manager", "image": "/team.jpg" },
-      "members": [
-        { "id": 302, "name": "Eva Black", "position": "Sales Representative", "image": "url_to_image" }
-      ]
-      }
-    ]
+    members: [
+      {
+        id: 202,
+        name: "Charlie Green",
+        position: "Content Writer",
+        image: "/team.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Sales",
+    teamLead: {
+      id: 301,
+      name: "David White",
+      position: "Sales Manager",
+      image: "/team.jpg",
+    },
+    members: [
+      {
+        id: 302,
+        name: "Eva Black",
+        position: "Sales Representative",
+        image: "url_to_image",
+      },
+    ],
+  },
+];
 
 export default teams;
