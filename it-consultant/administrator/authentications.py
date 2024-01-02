8e61a4d6bb128e87f1f927d1970e57f0cd5e6aa8
Olatide <olatidejoseph17@gmail.com>
2024-01-02 21:39:23 +0100

    fixed session auth for spectacular

--- it-consultant/administrator/authentications.py
+++ it-consultant/administrator/authentications.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from rest_framework.authentication import SessionAuthentication as SA
 
 class SessionAuthentication(SA):
@@ -6,3 +8,15 @@ class SessionAuthentication(SA):
 	"""
 	def enforce_csrf(self, request):
 		return
+
+class SessionAuthenticationScheme(OpenApiAuthenticationExtension):
+	target_class = 'administrator.authentications.SessionAuthentication'
+	name = 'cookieAuth'
+	priority = '-1'
+
+	def get_security_definition(self, auto_schema):
+	return {
+	    'type': 'apiKey',
+	    'in': 'cookie',
+	    'name': settings.SESSION_COOKIE_NAME,
+	}
